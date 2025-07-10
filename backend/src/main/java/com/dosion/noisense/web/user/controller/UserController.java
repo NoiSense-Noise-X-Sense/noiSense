c7cda662a8d1b7a68b4ada18a138fe4c08fb209d
juju <hellojuju558@gmail.com>
2025-07-10 16:25:15 +0900

    feat : User entity 추가
    - User entity 추가
    - BaseEntity 추가
    - /resources/repo DDL sql 파일 추가

--- backend/src/main/java/com/dosion/noisense/web/user/controller/UserController.java
+++ backend/src/main/java/com/dosion/noisense/web/user/controller/UserController.java
@@ -2,14 +2,17 @@ package com.dosion.noisense.web.user.controller;
 
 import com.dosion.noisense.web.user.dto.UserDto;
 import com.dosion.noisense.module.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/user") //변경
 @RequiredArgsConstructor
@@ -17,8 +20,11 @@ public class UserController {
 
   private final UserService userService;
 
+  @Operation(summary = "회원 생성")
   @PostMapping
-  public ResponseEntity<UserDto> getMyInfo(@Valid @RequestBody UserDto userDto) {
+  public ResponseEntity<UserDto> add(@RequestBody UserDto userDto) {
+    log.info("[controller ] userNm : " + userDto.getUserNm());
+    log.info("[controller ] nickname : " + userDto.getNickname());
     userService.add(userDto);
 //    return ResponseEntity.ok();
     return null;
