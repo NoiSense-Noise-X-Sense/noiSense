34b6e07fa9a0974f873c3c3b600b7847bc94a8ae
SCOM <scomjoo@gmail.com>
2025-07-11 11:22:48 +0900

    feat : 리포트 기능 구현 중(차트)

--- backend/src/main/java/com/dosion/noisense/module/report/repository/ReportRepository.java
+++ backend/src/main/java/com/dosion/noisense/module/report/repository/ReportRepository.java
@@ -1,7 +1,18 @@
 package com.dosion.noisense.module.report.repository;
 
+import com.dosion.noisense.web.report.dto.ComparisonChartDto;
+import com.dosion.noisense.web.report.dto.OverallChartDto;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface ReportRepository{
+public interface ReportRepository extends ChartDataRepository{
+
+
+  List<OverallChartDto> getOverallAvgData(String type, String startDate, String endDate, String autonomous);
+
+  List<ComparisonChartDto> getTrendPointAvgData(String type, String startDate, String endDate, List<String> trendPointRegion, String autonomous);
+
+
 }
--- backend/src/main/java/com/dosion/noisense/module/report/service/ReportService.java
+++ backend/src/main/java/com/dosion/noisense/module/report/service/ReportService.java
@@ -1,12 +1,15 @@
 package com.dosion.noisense.module.report.service;
 
 import com.dosion.noisense.module.report.repository.ReportRepository;
+import com.dosion.noisense.web.report.dto.OverallChartDto;
+import com.dosion.noisense.web.report.dto.TrendPointChartDto;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -14,7 +17,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ReportService {
 
-  //private final ReportRepository reportRepository;
+  private final ReportRepository reportRepository;
 
 
       /*
@@ -37,13 +40,57 @@ public class ReportService {
 
   public String getReport(String startDate, String endDate, String autonomousDistrict){
     log.info("getReport");
-    getChartData(startDate, endDate, autonomousDistrict, null);
+/*
+    // 데이터 목록들 요청
+
+    // 시끄러운, 조용한 지역 top3 데이터 요청
+    // 결과값에 따라서 구 또는 동 add
+    List<String> autonomousDistrictList = new ArrayList<>(); // 필터할 구
+    List<String> administrativeDistrictList = new ArrayList<>(); // 필터할 동
+    // 시끄러운, 조용한 지역 top3 차트 데이터 요청
+    // 검색값이 전체면 administrativeDistrictList은 null
+    getChartData(startDate, endDate, autonomousDistrictList, administrativeDistrictList);
+
+    // 기간 내 평균 소음 그래프 요청
+    getChartData(startDate, endDate, new ArrayList<>(List.of(autonomousDistrict)), null);
+
+
+
+*/
+
     return startDate + endDate + autonomousDistrict;
   }
 
-  private void getChartData(String startDate, String endDate, String autonomousDistrict, List<String> administrativeDistrictList){
+  // 차트 데이터 가져오기
+  private void getChartData(String startDate, String endDate, String autonomousDistrict){
+    // type : hourly, daily, dayOfWeek
+    // between startDate and endDate
+/*
+    String autonomous = autonomousDistrict.equals("all") ? null : autonomousDistrict;
+
+
+    // 1. 시간대 별 평균 소음
+    List<OverallChartDto> overallHourlyAvgNoiseData = reportRepository.getOverallAvgData("hourly", startDate, endDate, autonomous);
+    // 2. 일 별 평균 소음
+    List<OverallChartDto> overallDailyAvgNoiseData = reportRepository.getOverallAvgData("daily", startDate, endDate, autonomous);
+    // autonomous 가 null 이면 행정구 조건 없이 데이터를 가져온다
+
+    List<String> TrendPointRegion = null; // 시끄러운곳 행정구 또는 행정동 리스트 -> 메서드 변수값으로 받던지, 메서드로 가져옴
+    // 3. 시간대별
+    List<TrendPointChartDto> TrendPointHourlyAvgNoiseData = reportRepository.getTrendPointAvgData("hourly", startDate, endDate, TrendPointRegion, autonomous);
+    // 4. 요일별
+    List<TrendPointChartDto> TrendPointDayOfWeekAvgNoiseData = reportRepository.getTrendPointAvgData("DayOfWeek", startDate, endDate, TrendPointRegion, autonomous);
+    // autonomous 가 null 이면 TrendPointRegion은 행정구로 조건을 걸고
+    // null이 아니면 autonomous를 행정구 조건, TrendPointRegion 은 행정동 조건
+*/
+    // 데이터 가공 작업
     log.info("getChartData");
+
   }
+
+
+
+
 
 
 
