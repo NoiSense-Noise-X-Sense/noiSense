d678398639087f3c4fdfca8f7451217cdb145864
neisii <neisii@outlook.com>
2025-07-16 12:30:22 +0900

    feat. 자치구, 행정동 정보 조회 - 로컬 디비에 데이터 Insert용 테스트 케이스 포함

---
+++ backend/src/main/java/com/dosion/noisense/module/district/service/DistrictService.java
@@ -0,0 +1,35 @@
+package com.dosion.noisense.module.district.service;
+
+import com.dosion.noisense.module.district.entity.District;
+import com.dosion.noisense.module.district.repository.DistrictRepository;
+import com.dosion.noisense.web.district.dto.DistrictDto;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Slf4j
+@Service
+@AllArgsConstructor
+public class DistrictService {
+
+  private final DistrictRepository districtRepository;
+
+  public List<DistrictDto> getAllDistricts() {
+
+//    List<District> all = districtRepository.findAllDistricts();
+    List<District> autonomous = districtRepository.findAllAutonomousDistricts();
+    List<District> administrative = districtRepository.findAllAdministrativeDistricts();
+
+    List<DistrictDto> districtDtos = Stream.concat(autonomous.stream(), administrative.stream())
+      .map(District::toDto)
+      .collect(Collectors.toList());
+
+    log.info("distirctDtos : {}", districtDtos.size());
+
+    return districtDtos;
+  }
+}
