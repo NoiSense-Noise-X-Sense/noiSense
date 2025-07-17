b59f4e0cbda5cd6bce39f61f426d79b9d04b0fcb
neisii <neisii@outlook.com>
2025-07-17 14:39:20 +0900

    fix(db): 테이블이 public 스키마에 생성되는 문제 수정
    - noisense.noisense 스키마에 명시적으로 테이블 및 ENUM 생성되도록 수정
    - public 스키마에 생성되는 버그 방지 조치

--- Data/init/postgresql.repo.schema.init.sql
+++ Data/init/postgresql.repo.schema.init.sql
@@ -1,3 +1,6 @@
+SET search_path TO noisense;
+
+
 CREATE TYPE boundary_type AS ENUM ('AUTONOMOUS_DISTRICT', 'ADMINISTRATIVE_DISTRICT');
 CREATE TYPE geometry_format AS ENUM ('GeoJSON', 'WKT');
 CREATE TYPE geometry_type AS ENUM ('MultiPolygon', 'Polygon');
