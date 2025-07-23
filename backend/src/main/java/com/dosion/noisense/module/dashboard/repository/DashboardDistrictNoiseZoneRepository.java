504dac00a39e055c3bbabdf8b87273e6761be81c
soheejjang0912 <66730047+soheejjang0912@users.noreply.github.com>
2025-07-23 12:21:15 +0900

    chore : 불필요 로그, 코드 수정 및 삭제

--- backend/src/main/java/com/dosion/noisense/module/dashboard/repository/DashboardDistrictNoiseZoneRepository.java
+++ backend/src/main/java/com/dosion/noisense/module/dashboard/repository/DashboardDistrictNoiseZoneRepository.java
@@ -8,8 +8,6 @@ import java.util.List;
 
 public interface DashboardDistrictNoiseZoneRepository
   extends JpaRepository<DashboardDistrictNoiseZone, DashboardDistrictNoiseZoneId> {
-
-
   // 자치구별 평균 소음 내림차순 조회
   List<DashboardDistrictNoiseZone> findByAutonomousDistrictCodeOrderByAvgNoiseDesc(String autonomousDistrictCode);
 }
