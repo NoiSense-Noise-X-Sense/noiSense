ef85736842f4d57fbcaad4a32400502fd86482a6
SCOM <scomjoo@gmail.com>
2025-07-26 22:11:29 +0900

    feat : 감정계수 추가

--- backend/src/main/java/com/dosion/noisense/module/board/repository/BoardRepository.java
+++ backend/src/main/java/com/dosion/noisense/module/board/repository/BoardRepository.java
@@ -2,11 +2,14 @@ package com.dosion.noisense.module.board.repository;
 
 import com.dosion.noisense.web.board.dto.BoardDto;
 import com.dosion.noisense.module.board.entity.Board;
+import com.dosion.noisense.web.report.dto.EmotionBoardDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
